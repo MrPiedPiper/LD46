04181446adea6b367b27d3406febe5d14a143c61
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2020-04-18 15:23:33 -0700

    Spend wayyyyy too long on a play UI

--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -8,18 +8,12 @@ var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	ui_play.set_inventory_max(player.inventory_size)
 
 func _on_Player_got_score(new_score):
 	score = new_score
 	ui_play.set_score(score)
 
 func _on_Player_got_item(new_item):
-	$UI/Play_UI.set_inventory_curr(player.inventory_list.size())
-	$UI/Play_UI.set_inventory_max(player.inventory_size)
+	ui_play.set_inventory_curr(player.inventory_list.size())
+	ui_play.set_inventory_max(player.inventory_size)
--- scenes/ui/play_ui/Play_UI.gd
+++ scenes/ui/play_ui/Play_UI.gd
@@ -1,14 +1,38 @@
 extends Control
 
-onready var score_label = $MarginContainer/MarginContainer/VBoxContainer/HBoxContainer/Label2
-onready var inventory_curr = $MarginContainer/MarginContainer/VBoxContainer/HBoxContainer2/Label3
-onready var inventory_max = $MarginContainer/MarginContainer/VBoxContainer/HBoxContainer2/Label2
+onready var inventory_count_parent = $MarginContainer/MarginContainer/HBoxContainer/HBoxContainer2/InventoryCountHBoxContainer
+onready var inventory_size_parent = $MarginContainer/MarginContainer/HBoxContainer/HBoxContainer2/InventorySizeHBoxContainer
+onready var score_count_parent = $MarginContainer/MarginContainer/HBoxContainer/HBoxContainer/ScoreCountHBoxContainer
 
-func set_score(new_text):
-	score_label.text = str(new_text)
+export var number_texture:Texture
 
-func set_inventory_curr(new_text):
-	inventory_curr.text = str(new_text)
+var number_dict = {}
+
+func _ready():
+	for i in range(0,10):
+		var new_image = AtlasTexture.new()
+		new_image.atlas = number_texture
+		var number_position = Vector2(6*i,0)
+		var number_size = Vector2(6,10)
+		new_image.region = Rect2(number_position, number_size)
+		number_dict[str(i)] = new_image
+
+func set_score(new_score):
+	add_numbers(new_score,score_count_parent)
+
+func set_inventory_curr(new_count):
+	add_numbers(new_count,inventory_count_parent)
 	
-func set_inventory_max(new_text):
-	inventory_max.text = str(new_text)
+func set_inventory_max(new_count):
+	add_numbers(new_count,inventory_size_parent)
+
+func add_numbers(number:int,parent_node):
+	for i in parent_node.get_children():
+		print(i)
+		i.queue_free()
+	var number_string = str(number)
+	for i in number_string:
+		var new_texture = TextureRect.new()
+		new_texture.size_flags_vertical = SIZE_SHRINK_CENTER
+		new_texture.texture = number_dict[i]
+		parent_node.add_child(new_texture)
